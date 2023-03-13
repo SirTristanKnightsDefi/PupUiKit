905022fb84226c519c0e6f04e63ba0d3f5a0c80e
Sir Tristan <sirtris@knightsdefi.com>
2023-03-13 08:52:53 -0400

    color update

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -3191,17 +3191,17 @@ var baseColors = {
     primary: "#01BDC6",
     primaryBright: "#01BDC6",
     primaryDark: "#028E9B",
-    secondary: "#01BDC6",
+    secondary: "#FC8600",
     success: "#15bd69",
     warning: "#fa113e",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f1f6f8", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeeee", tertiary: "#01BDC6", text: "#028E9B", textDisabled: "#BDC2C4", textSubtle: "#028E9B", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f1f6f8", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeeee", tertiary: "#EFF4F5", text: "#028E9B", textDisabled: "#BDC2C4", textSubtle: "#028E9B", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#01BDC6", background: "#100C18", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#01BDC6", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#EAE2FC", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#05FC9C", background: "#100C18", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#EAE2FC", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
